5e08c1c9f1c2cc1eb7d9aaaa6fabc184a195b2fc
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-01-24 17:15:18 +0100

    modification d ela class game, ajout de la methode joinGame

--- game.fs
+++ game.fs
@@ -1,30 +1,50 @@
 ﻿module game
 
- // Définition classe Personne 
-//type Personne = class
-    // attributs     val private nom : string
- //   val private prénom : string
-  //  val private âge : int
-    // surcharges constructeurs et valeurs par défaut
-   // new(n) = {nom=n; prénom="?"; âge=(-1)} //valeurs par défaut
-   // new(n,p) = {nom=n; prénom=p; âge=(-1)} //valeur par défaut
-    //new(n,p,â) = {nom=n; prénom=p; âge=â}
-    //new(n,p,dateN) = {nom=n; prénom=p;âge=calculÂge(dateN)}
-    // accesseurs
- //   member this.Nom = this.nom
-   // member this.Prénom = this.prénom
-    //member this.Âge = this.âge
-//end
-
+//objet métier game
+//int id 
+//string key 
+//Stringstatus (WAITING:Attente second joueur, 
+//TURN_PLAYER_1:tour joueur1, TURN_PLAYER_2: tour joueur2, PLAYER_1_WON:joueur 1 gagne,PLAYER_2_WON:joueur 2 gagne )
+//String gridOrientation “vertical” ou “horizontal” array [42] 
+//string gridCells,
+//les string valent “no” si vide, 
+//“p1” si jeton du joueur1, “p2” si jeton du joueur 2 
 
 type Game = class
-   val private playerName1 : string
-   val private playerName2 : string
-   val private gameKey : string
-   //val private verticalH : bool
+   val mutable gameId : int
+   val mutable gameStatus : string
+   //gameStatus=[|"WAITING"; "TURN_PLAYER_1"; "TURN_PLAYER_2";"PLAYER_1_WON"; "PLAYER_2_WON";|];
+   val mutable gridOrientation : string
+   val mutable gridCells : string[]
+   val mutable playerName1 : string
+   val mutable playerName2 : string
+   val mutable gameKey : string
 
-   // surcharges constructeurs et valeurs par défaut
-    new() = {playerName1="?"; playerName2="?"; gameKey="?"} //valeurs par défaut
-    new(j1:string, key:string) = {playerName1=j1; playerName2="?"; gameKey=key} //valeurs par défaut
+   //les constructeurs , par défaut et surchargés
+   new() as this =
+      { gameId = (-1); gameStatus = "?"; gridOrientation = "?"; gridCells = Array.create 41 "no";playerName1="?";playerName2 ="?"; gameKey="?"}
+
+   new(id, statu,  orientation,p1,key) as this =
+      { gameId = id; gameStatus = statu; gridOrientation = orientation;gridCells = Array.create 41 "no"; playerName1 = p1;playerName2 ="?"; gameKey = key;}
+      then
+         printfn " Objet créé avec: {(gameid : %i, player1 :%s), ( gameKey : %s , orientation grid :%s)}"
+            this.gameId this.playerName1 this.gameKey this.gridOrientation 
+
+   //Methode Join, permet a un joueur 2 de rejoindre une partie
+   // Vérifier que nom different du createur (!) 
+   //ajoute nom du joueur 2 à l’objet game. choisis qui commence et met l’état correspondant 
+   member this.joinGame (p2:string, key:string) :unit= 
+      if not(p2.Equals( this.playerName1)) then
+          this.playerName2 <- p2
+      else
+          printf "Les deux joueurs ne doivent pas avoir le même pseudo"
+   //Methode gravité, qui va faire descendre un pion au dernier utilisé
+
+
+    //Methode turnToLeft, qui tourne le tableau et applique la gravité
+
+    //Methode turnToRight, qui tourne le tableau et applique la gravité
+
+    //Methode recursive checkGrid, verifie si il y a une ligne de 4
 
 end

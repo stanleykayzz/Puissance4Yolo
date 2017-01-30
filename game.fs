879bac3da12bc8f2f90312847b7f7d18161a4319
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-01-30 16:30:10 +0100

    ajout des constructeurs par defaut et surchargé

--- game.fs
+++ game.fs
@@ -8,4 +8,13 @@ type Game = class
    val mutable playerName1 : string
    val mutable playerName2 : string
    val mutable gameKey : string
+    //les constructeurs , par défaut et surchargés
+   new() as this =
+      { gameId = (-1); gameStatus = "?"; gridOrientation = "?"; gridCells = Array.create 42 "no";playerName1="?";playerName2 ="?"; gameKey="?"}
+
+   new(id, statu,  p1,key) as this =
+      { gameId = id; gameStatus = statu; gridOrientation = "Verticale";gridCells = Array.create 42 "no"; playerName1 = p1;playerName2 ="?"; gameKey = key;}
+      then
+         printfn " Objet créé avec: {(gameid : %i, player1 :%s), ( gameKey : %s , orientation grid :%s)}"
+            this.gameId this.playerName1 this.gameKey this.gridOrientation 
 end

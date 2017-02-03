593dfa0c0e35c5331cdc228d58e635562e6af553
Alvin Ondzounga <alvin.ondzounga@avanade.com>
2017-02-03 15:07:57 +0100

    ajout des types join, action et information en plus du type create

--- customTypes.fs
+++ customTypes.fs
@@ -1,7 +1,30 @@
 ﻿module customTypes
+
 type create = {
     player1Key : string
     gameKey: string
+}
+
+type join = {
+   player2Key: string
+}
+
+type information = {
+  key : string
+  status : string
+  player1Name:string
+  player2Name:string
+  gridOrientation:string
+  gridCells : string[]
+}
+
+type action = {
+  key : string
+  status : string
+  player1Name:string
+  player2Name:string
+  gridOrientation:string
+  gridCells : string[]
 }
 
 
